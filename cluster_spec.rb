3be5fb679ac65a067372e850ca126adc10b436db
breadbaker <dan@innovationdept.com>
2015-04-24 11:15:16 -0400

    cluster spec

--- cluster_spec.rb
+++ cluster_spec.rb
@@ -1,8 +1,27 @@
-require
-
 require 'rspec'
+require './cluster'
 
-RSpec.describ Cluster, "assignment" do
-  context "two different clusters" do
-    it "know which cluster" do
-      cluster = 
+describe Cluster  do
+  before :each do
+    @cluster = Cluster.new
+  end
+  describe "create_center" do
+      # def create_center(points)
+      #   points.times do |i|
+      #     @center << Random.rand()
+      #   end
+      # end
+      before :each do 
+        @points = 10
+        @cluster.create_center(@points)
+      end
+      it "creates n points" do
+        @cluster.center.length.should eq @points
+      end
+      it "points are floats" do
+        @cluster.center.each do |pt|
+          pt.class.should eq Float
+        end
+      end
+  end
+end
